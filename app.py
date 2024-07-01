77a951b90d90c0ed16ea7bb7611719f10eb1e8db
Adson Felipe <59034291+AdsonF7@users.noreply.github.com>
2024-07-01 03:27:46 -0300

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,30 @@ class App:
   
   def __init__(self):
 
-  def save_file(self):
+  def rename_file(self, old_path, new_name):
     """
-      method used to save file
+      method used to rename file
+
+      Args:
+        old_path (WindowPath): 
+        new_name (str): New file name
+
+      Returns:
+        boolean: if there is no error, it returns true
     """
+    os.rename(old_path, pathlib.PurePath(old_path.parent, new_name + old_path.suffix))
+
+  def set_tracknumber(self, pathfile, tracknumber):
+    """
+      method to change tracknumber
+
+      Args:
+        pathfile (WindowsPath): 
+        tracknumber (int): new tracknumber audio file
+        
+      Returns:
+        boolean: if there is no error, it returns true
+    """
+    audio = EasyID3(file)
+    audio["tracknumber"] = tracknumber
+    audio.save()
